c161bef03a7a648a19a19b22b9960f040e53e546
Harshna Goratela <harshna.goratela@gmail.com>
2021-12-25 21:17:48 +0530

    fixed meta tag title

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,8 +3,8 @@ import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 function SEO({ title, description, image }) {
-    const { site, imageSharp } = useStaticQuery(
-        graphql`
+  const { site, imageSharp } = useStaticQuery(
+    graphql`
        query {
          site {
            siteMetadata {
@@ -21,49 +21,50 @@ function SEO({ title, description, image }) {
           }
        }
      `
-    )
+  )
 
-    const defaultTitle = site.siteMetadata?.title
-    const metaTitle = title || site.siteMetadata?.title;
-    const metaDescription = description || site.siteMetadata?.description;
-    const metaAuthor = site.siteMetadata?.author || '';
-    const metaUrl = site.siteMetadata?.url || '';
-    const metaImage = image || imageSharp?.fluid.src;
+  const defaultTitle = site.siteMetadata?.title
+  const metaTitle = title || site.siteMetadata?.title;
+  const metaDescription = description || site.siteMetadata?.description;
+  const metaAuthor = site.siteMetadata?.author || '';
+  const metaUrl = site.siteMetadata?.url || '';
+  const metaImage = image || imageSharp?.fluid.src;
 
-    return (
-        <Helmet
-            title={metaTitle}
-            titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
-        >
-            {/* General tags */}
-            <title>{metaTitle}</title>
-            <meta name="description" content={metaDescription} />
-            <meta name="image" content={metaImage} />
+  return (
+    <Helmet
+      title={metaTitle}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+    >
+      {/* General tags */}
+      <title>{metaTitle}</title>
+      <meta name="title" content={metaTitle} />
+      <meta name="description" content={metaDescription} />
+      <meta name="image" content={metaImage} />
 
-            {/* OpenGraph tags */}
-            <meta property="og:url" content={metaUrl} />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={metaTitle} />
-            <meta property="og:description" content={metaDescription} />
-            <meta property="og:image" content={metaImage} />
+      {/* OpenGraph tags */}
+      <meta property="og:url" content={metaUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:image" content={metaImage} />
 
-            {/* Twitter Card tags */}
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content={metaTitle} />
-            <meta name="twitter:description" content={metaDescription} />
-            <meta name="twitter:creator" content={metaAuthor} />
-            <meta name="twitter:image" content={metaImage} />
-            <script type="text/javascript">
-            {`
+      {/* Twitter Card tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:creator" content={metaAuthor} />
+      <meta name="twitter:image" content={metaImage} />
+      <script type="text/javascript">
+        {`
               (function() { // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
               var d = document, s = d.createElement('script'); // IMPORTANT: Replace EXAMPLE with your forum shortname!
               s.src = 'https://devengoratela.disqus.com/recommendations.js'; s.setAttribute('data-timestamp', +new Date());
               (d.head || d.body).appendChild(s);
               })();
             `}
-			      </script>
-        </Helmet>
-    )
+      </script>
+    </Helmet>
+  )
 }
 
 export default SEO
