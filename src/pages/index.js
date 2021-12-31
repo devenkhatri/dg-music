6f4306e795ffeff54b2c69bfa462519ce06cf395
Deven Goratela <deven.khatri@gmail.com>
2021-12-31 21:49:49 +0530

    formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Header, Card, Icon, Image } from 'semantic-ui-react'
+import { Header, Card, Icon, Image, Label, Menu } from 'semantic-ui-react'
 import { graphql } from 'gatsby';
 import SongPlayer from '../components/songplayer';
 import { useGlobal } from '../globalstore';
+import _ from 'lodash';
+import Moment from 'react-moment';
+
 
 export default function Home({ data }) {
-  const allRecordings = (data.allRecordings && data.allRecordings.edges) || []
+  let allRecordings = (data.allRecordings && data.allRecordings.group) || []
+  allRecordings = _.orderBy(allRecordings, ['fieldValue'], ['desc'])
+
   const [globalState, globalActions] = useGlobal();
   const [reload, setReload] = useState(0);
 
-  useEffect(()=>{
-    setReload(1)    
+  useEffect(() => {
+    setReload(1)
     globalActions.getPlayCountsAirtable();
-  },[reload]);
+  }, [reload]);
   return (
     <Layout>
       <SEO title="Recordings" />
@@ -22,31 +27,54 @@ export default function Home({ data }) {
         <Icon name='soundcloud' circular inverted color="blue" />
         <Header.Content>List of Recordings</Header.Content>
       </Header>
-      <Card.Group doubling itemsPerRow={3} stackable>
-        {allRecordings.map(({ node }) => (
-          <Card key={node.recordId}>
-            <a href={`/recording/${node.fields.slug}`}>
-              <Image
-                src={node.data.CoverImage && node.data.CoverImage.localFiles && node.data.CoverImage.localFiles[0].childImageSharp.fluid.src} fluid
-                style={{ height: '13rem', objectFit: 'cover' }}
-              />
-            </a>
-            <Card.Content>
-              <Card.Header as="a" href={`/recording/${node.fields.slug}`}>{node.data.SongTitle}</Card.Header>
-              <Card.Meta>
-                <span className='date'>{node.data.RecordingDate}</span>
-              </Card.Meta>
-              <Card.Description>
-                <SongPlayer src={node.data.MediaFile && node.data.MediaFile[0] && node.data.MediaFile[0].url} isAutoPlay={false} layout="stacked-reverse" recordId={node.recordId} />
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Icon name='play' />
-                {globalActions.getPlayCount(node.recordId)}
-            </Card.Content>
-          </Card>
-        ))}
-      </Card.Group>
+
+      {allRecordings && allRecordings.map((item) => (
+        <>
+          <Header>
+            <Menu compact>
+              <Menu.Item as='a'>
+                <Icon name='calendar' />
+                <Moment format="MMM DD, YYYY">
+                  {item.fieldValue}
+                </Moment>
+                <Label color='blue' floating circular>
+                  {item.edges.length}
+                </Label>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          {/* <Label ribbon as="a" color="blue" style={{padding: '1rem', margin: '2rem 0 0 1rem'}}>
+            <Moment format="MMM DD, YYYY">
+              {item.fieldValue}
+            </Moment>
+          </Label> */}
+          <Card.Group itemsPerRow={3} stackable>
+            {item.edges.map(({ node }) => (
+              <Card key={node.recordId}>
+                <a href={`/recording/${node.fields.slug}`}>
+                  <Image
+                    src={node.data.CoverImage && node.data.CoverImage.localFiles && node.data.CoverImage.localFiles[0].childImageSharp.fluid.src} fluid
+                    style={{ height: '13rem', objectFit: 'cover' }}
+                  />
+                </a>
+                <Card.Content>
+                  <Card.Header as="a" href={`/recording/${node.fields.slug}`}>{node.data.SongTitle}</Card.Header>
+                  <Card.Meta>
+                    <span className='date'>{node.data.RecordingDate}</span>
+                  </Card.Meta>
+                  <Card.Description>
+                    <SongPlayer src={node.data.MediaFile && node.data.MediaFile[0] && node.data.MediaFile[0].url} isAutoPlay={false} layout="stacked-reverse" recordId={node.recordId} />
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Icon name='play' />
+                  {globalActions.getPlayCount(node.recordId)}
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        </>
+      ))}
       <div id="disqus_recommendations"></div>
     </Layout>
   );
@@ -55,28 +83,31 @@ export default function Home({ data }) {
 export const query = graphql`
 query {
   allRecordings: allAirtable(
-    sort: {fields: data___RecordingDate, order: DESC}
+    sort: {fields: data___SongTitle}
     filter: {table: {eq: "Recordings"}}
   ) {
-    edges {
-      node {
-        recordId
-        fields {
-          slug
-        }
-        data {
-          SongTitle
-          Singer
-          Plays
-          RecordingDate(formatString: "MMM DD, YYYY")
-          MediaFile {
-            url
+    group(field: data___RecordingDate) {
+      fieldValue
+      edges {
+        node {
+          recordId
+          fields {
+            slug
           }
-          CoverImage {
-            localFiles {
-              childImageSharp {
-                fluid {
-                  src
+          data {
+            SongTitle
+            Singer
+            Plays
+            RecordingDate(formatString: "MMM DD, YYYY")
+            MediaFile {
+              url
+            }
+            CoverImage {
+              localFiles {
+                childImageSharp {
+                  fluid {
+                    src
+                  }
                 }
               }
             }
--- src/pages/statistics.js
+++ src/pages/statistics.js
@@ -4,72 +4,88 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { useGlobal } from '../globalstore';
 import { graphql } from 'gatsby';
+import _ from 'lodash';
+import Moment from 'react-moment';
 
 const SongStatistics = ({ data }) => {
-    const allRecordings = (data.allRecordings && data.allRecordings.edges) || []
-    const [globalState, globalActions] = useGlobal();
-    const [grandTotal, setGrandTotal] = useState(0);
+  let allRecordings = (data.allRecordings && data.allRecordings.group) || []
+  allRecordings = _.orderBy(allRecordings, ['fieldValue'], ['desc'])
 
-    useEffect(() => {
-        if (globalState.playCounts && globalState.playCounts.length <= 0) {
-            globalActions.getPlayCountsAirtable().then(() => {
-                initialize();
-            });
-        } else {
-            initialize();
-        }
-    }, [grandTotal]);
+  const [globalState, globalActions] = useGlobal();
+  const [grandTotal, setGrandTotal] = useState(0);
 
-    const initialize = () => {
-        let total = 0;
-        globalState.playCounts && globalState.playCounts.map((item) => {
-            total += item.count;
-        })
-        setGrandTotal(total)
+  useEffect(() => {
+    if (globalState.playCounts && globalState.playCounts.length <= 0) {
+      globalActions.getPlayCountsAirtable().then(() => {
+        initialize();
+      });
+    } else {
+      initialize();
     }
+  }, [grandTotal]);
+
+  const initialize = () => {
+    let total = 0;
+    globalState.playCounts && globalState.playCounts.map((item) => {
+      total += item.count;
+    })
+    setGrandTotal(total)
+  }
 
 
-    return (
-        <Layout>
-            <SEO title="Recordings" />
-            <Header as='h1' icon textAlign='center'>
-                <Icon name='chart bar' circular inverted color="blue" />
-                <Header.Content>Songs Statistics</Header.Content>
-            </Header>
-            <Container textAlign="center">
-                <Segment placeholder>
-                    <Statistic color='blue' size='huge'>
-                        <Statistic.Value>{grandTotal}</Statistic.Value>
-                        <Statistic.Label style={{ paddingTop: '1rem' }}>Total times songs played</Statistic.Label>
-                    </Statistic>
-                </Segment>
-                <Label size="massive" color="blue">
-                    No. of times Songs played
-                </Label>
-                <Table color="blue" size="large" stackable compact>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Song Title</Table.HeaderCell>
-                            <Table.HeaderCell>Song Date</Table.HeaderCell>
-                            <Table.HeaderCell textAlign='center'>No of time played</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {allRecordings && allRecordings.map(({ node }) => (
-                            <Table.Row key={node.recordId}>
-                                <Table.Cell>{node.data.SongTitle}</Table.Cell>
-                                <Table.Cell>{node.data.RecordingDate}</Table.Cell>
-                                <Table.Cell textAlign='center'>
-                                    <Icon name='music' />
-                                    {globalActions.getPlayCount(node.recordId)}
-                                </Table.Cell>
-                            </Table.Row>
-                        ))}
-                    </Table.Body>
-                </Table>
-            </Container>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <SEO title="Statistics" />
+      <Header as='h1' icon textAlign='center'>
+        <Icon name='chart bar' circular inverted color="blue" />
+        <Header.Content>Songs Statistics</Header.Content>
+      </Header>
+      <Container textAlign="center">
+        <Segment placeholder>
+          <Statistic color='blue' size='huge'>
+            <Statistic.Value>{grandTotal}</Statistic.Value>
+            <Statistic.Label style={{ paddingTop: '1rem' }}>Total times songs played</Statistic.Label>
+          </Statistic>
+        </Segment>
+        <Label size="massive" color="blue">
+          No. of times Individual Songs played
+        </Label>
+        <Table color="blue" size="large" stackable compact>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Song Title</Table.HeaderCell>
+              {/* <Table.HeaderCell>Recording Date</Table.HeaderCell> */}
+              <Table.HeaderCell textAlign='left'>No of time played</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {allRecordings && allRecordings.map((item) => (
+              <>
+                <Table.Row>
+                  <Table.Cell colSpan="3" warning textAlign="center">
+                    <Moment format="MMM DD, YYYY">
+                      {item.fieldValue}
+                    </Moment>
+                  </Table.Cell>
+                </Table.Row>
+                {item.edges.map(({ node }) => (
+                  <Table.Row key={node.recordId}>
+                    <Table.Cell>{node.data.SongTitle}</Table.Cell>
+                    {/* <Table.Cell>{node.data.RecordingDate}</Table.Cell> */}
+                    <Table.Cell textAlign='left'>
+                      <Icon name='music' />
+                      {globalActions.getPlayCount(node.recordId)}
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </>
+            ))}
+
+          </Table.Body>
+        </Table>
+      </Container>
+    </Layout>
+  );
 }
 
 export default SongStatistics;
@@ -77,31 +93,20 @@ export default SongStatistics;
 export const query = graphql`
 query {
   allRecordings: allAirtable(
-    sort: {fields: data___RecordingDate, order: DESC}
     filter: {table: {eq: "Recordings"}}
+    sort: {fields: data___SongTitle}
   ) {
-    edges {
-      node {
-        recordId
-        fields {
-          slug
-        }
-        data {
-          SongTitle
-          Singer
-          Plays
-          RecordingDate(formatString: "MMM DD, YYYY")
-          MediaFile {
-            url
+    group(field: data___RecordingDate) {
+      fieldValue
+      edges {
+        node {
+          recordId
+          fields {
+            slug
           }
-          CoverImage {
-            localFiles {
-              childImageSharp {
-                fluid {
-                  src
-                }
-              }
-            }
+          data {
+            SongTitle
+            RecordingDate(formatString: "MMM DD, YYYY")
           }
         }
       }
